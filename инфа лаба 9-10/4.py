dc3e02aba1305c5c5bc3d78d74ece4f50cd05660
xXSafireXx <117628338+xXSafireXx@users.noreply.github.com>
2022-11-28 17:55:23 +0300

    Add files via upload

--- инфа лаба 9-10/4.py
+++ инфа лаба 9-10/4.py
@@ -2,21 +2,25 @@ a=list(map(int,input().split()))
 b=list(map(int,input().split()))
 print(a)
 print(b)
+def o(k):
+    l=0
+    for i in range(len(k)):
+     if k[i]>0:
+        l+=k[i]
+        return(l)
+def p(k):
+    for i in range(len(k)):
+                           k[i]=k[i]*10
+    print(k)
+    
 x=0
 y=0
 i=0
-for i in range(len(a)):
-    if a[i]>0:
-        x+=a[i]
+x=o(a)
 i=0
-for i in range(len(b)):
-    if b[i]>0:
-        y+=b[i]
+y=o(b)
 if x>y:
-    for i in range(len(b)):
-                           b[i]=b[i]*10
-    print(b)
-if x<y:
-                   for i in range(len(a)):
-                           a[i]=a[i]*10
-                   print(a)
+    p(b)
+else:
+    p(a)
+
